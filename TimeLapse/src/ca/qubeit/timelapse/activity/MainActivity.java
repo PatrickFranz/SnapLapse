24fac03ba167fe678141007f7d9cf06b1a6a9f62
PatrickFranz <clymbing@gmail.com>
2014-03-03 14:37:01 -0800

    A few more tweaks to the menu.

--- TimeLapse/src/ca/qubeit/timelapse/activity/MainActivity.java
+++ TimeLapse/src/ca/qubeit/timelapse/activity/MainActivity.java
@@ -1,7 +1,8 @@
 package ca.qubeit.timelapse.activity;
 
 import android.app.Activity;
-import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
@@ -16,6 +17,8 @@ public class MainActivity extends Activity {
 	private LinearLayout btnOpen;
 	private LinearLayout btnRemove;
 	private LinearLayout btnView;
+	private Drawable buttonUp;
+	private Drawable buttonDown;
 	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,20 +29,25 @@ public class MainActivity extends Activity {
         btnOpen = (LinearLayout)findViewById(R.id.btn_open);
         btnRemove = (LinearLayout)findViewById(R.id.btn_remove);
         btnView = (LinearLayout)findViewById(R.id.btn_view);
+        buttonUp = (Drawable)getResources().getDrawable(R.drawable.menu_button_shape);
+        buttonDown = (Drawable)getResources().getDrawable(R.drawable.menu_button_shape_down);
+        
         
         //Set listeners on buttons
         btnCreate.setOnClickListener(new OnClickListener() {
-			
-			@Override
+        	@Override
 			public void onClick(View v) {
+        		btnCreate.setBackgroundDrawable(buttonDown);
 				Toast.makeText(getBaseContext(), "Create a project", Toast.LENGTH_SHORT)
 				.show();				
 			}
 		});
         
+        
         btnOpen.setOnClickListener(new OnClickListener() {
 			@Override
 			public void onClick(View v) {
+				btnOpen.setBackgroundDrawable(buttonDown);
 				Toast.makeText(getBaseContext(), "Open project", Toast.LENGTH_SHORT)
 				.show();				
 			}
@@ -48,6 +56,7 @@ public class MainActivity extends Activity {
         btnRemove.setOnClickListener(new OnClickListener() {
 			@Override
 			public void onClick(View v) {
+				btnRemove.setBackgroundDrawable(buttonDown);
 				Toast.makeText(getBaseContext(), "Remove a project", Toast.LENGTH_SHORT)
 				.show();
 			}
@@ -56,12 +65,11 @@ public class MainActivity extends Activity {
         btnView.setOnClickListener(new OnClickListener() {
 			@Override
 			public void onClick(View v) {
+				btnView.setBackgroundDrawable(buttonDown);
 				Toast.makeText(getBaseContext(), "View project", Toast.LENGTH_SHORT)
-				.show();				
+				.show();
 			}
 		});
-        
-        Intent i = new Intent(this, TakePicture.class);
     }
 
 
