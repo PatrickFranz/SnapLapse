b0c62eb9f69ea03ca5ac1428379ae721462184ab
PatrickFranz <clymbing@gmail.com>
2014-03-11 16:31:58 -0700

    Change Launcher Icon

---
+++ TimeLapse/src/ca/qubeit/snaplapse/util/CameraHelper.java
@@ -0,0 +1,30 @@
+package ca.qubeit.snaplapse.util;
+
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.hardware.Camera;
+import android.util.Log;
+
+public class CameraHelper {
+
+	private static final String TAG = "CameraHelper";
+	
+	public static boolean checkCameraHardware(Context ctx){
+		if(ctx.getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA_ANY)){
+			return true;
+		} else {
+			return false;
+		}
+	}
+	
+	public static Camera getCameraInstance(int cameraId){
+		Camera camera = null;
+		try{
+			camera = Camera.open(cameraId);
+		} catch (Exception e) {
+			Log.e(TAG, "Camera not available");
+		}
+		
+		return camera;
+	}
+}
---
+++ TimeLapse/src/ca/qubeit/snaplapse/view/CameraPreview.java
@@ -0,0 +1,68 @@
+package ca.qubeit.snaplapse.view;
+
+import java.io.IOException;
+
+import android.content.Context;
+import android.hardware.Camera;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+
+public class CameraPreview extends SurfaceView implements SurfaceHolder.Callback{
+	private static final String TAG = "CameraPreview";
+	private Camera camera;
+	private SurfaceHolder holder;
+	
+	public CameraPreview(Context context, AttributeSet attrs, int defStyle) {
+		super(context, attrs, defStyle);
+	}
+	
+
+	public CameraPreview(Context context, AttributeSet attrs) {
+		super(context, attrs);
+	}
+
+
+	public CameraPreview(Context context) {
+		super(context);
+	}
+
+	public void init(Camera camera){
+		this.camera = camera;
+		initSurfaceHolder();
+	}
+	
+	private void initSurfaceHolder(){
+		holder = getHolder();
+		holder.addCallback(this);
+	}
+	
+	private void initCamera(SurfaceHolder holder){
+		try {
+			camera.setPreviewDisplay(holder);
+			camera.startPreview();
+		} catch (IOException e) {
+			Log.e(TAG, "Error setting camera preview...");
+			e.printStackTrace();
+		}
+		
+	}
+	
+	@Override
+	public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+		
+	}
+
+	@Override
+	public void surfaceCreated(SurfaceHolder holder) {
+		initCamera(holder);
+		
+	}
+
+	@Override
+	public void surfaceDestroyed(SurfaceHolder holder) {
+		
+	}
+
+}
